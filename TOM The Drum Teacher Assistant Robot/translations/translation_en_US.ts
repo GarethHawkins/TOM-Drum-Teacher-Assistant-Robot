1f36322f66cffc3e0cf9a761efbd11b31061bfe9
GarethHawkins <38431599+GarethHawkins@users.noreply.github.com>
2020-05-16 14:31:13 +0100

    Master Project Files

---
+++ TOM The Drum Teacher Assistant Robot/translations/translation_en_US.ts
@@ -0,0 +1,27 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>behavior_1/behavior.xar:/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Peak Heard</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Peak Heard</translation>
+        </message>
+        <message>
+            <source>Peak</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Peak</translation>
+        </message>
+        <message>
+            <source>THUD</source>
+            <comment>Text</comment>
+            <translation type="obsolete">THUD</translation>
+        </message>
+    </context>
+</TS>
